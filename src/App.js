53bf71dfcbbfbc2b0bf390683cc6a4ab18a29278
jayedeep <jaydeeptalaviya8@gmail.com>
2020-09-16 10:04:10 +0530

    second_game

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import "./App.css";
 import Game from "./Game";
 import Logic from "./Logic";
 import ProgressBar from "./ProgressBar";
+import { Route, Switch } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import NewGameAgain from "./components/NewGameAgain";
+import Home from "./Home";
 
 function App() {
   return (
     <div>
-      <Logic />
+      {/* <Logic /> */}
+      <Dashboard />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/Game1" component={Logic}></Route>
+        <Route exact path="/Game2" component={NewGameAgain}></Route>
+      </Switch>
     </div>
   );
 }
